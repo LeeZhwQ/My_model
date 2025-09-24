bc8b1fb4f1ba9cf05894f74dc1d155c447fb6aa1
ZhengHao Li <leonliqiuqiu@gmail.com>
2025-09-24 21:09:18 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,34 +11,35 @@ import pandas as pd
 
 #Hyperparameters
 d_model = 512
-context_length = 32
+context_length = 128
 num_heads = 8
 head_size = d_model // num_heads
 device = 'cuda' if torch.cuda.is_available() else "cpu"
 dropout = 0.1
 num_blocks = 12
 batch_size = 12
-max_steps = 1000
+max_steps = 100000
 learning_rate = 1e-3
 eval_interval = 20
 eval_iters = 5
 TORCH_SEED = 1072
 torch.manual_seed(TORCH_SEED)
 
-df = pd.read_csv("ChnSentiCorp_htl_all.csv")
+
+df = pd.read_csv("/home/zhenghao/My_model/ChnSentiCorp_htl_all.csv")
 all_reviews = df["review"].tolist()  
 text = ";".join(str(review) for review in all_reviews)
 
 tokenizer = tiktoken.get_encoding("cl100k_base")
 tokenized_text = tokenizer.encode(text)
-tokenizer_text = torch.Tensor(tokenized_text,dtype = torch.long ,device=device)
+tokenizer_text = torch.tensor(tokenized_text,dtype = torch.long ,device=device)
 
 train_size = int(len(text) * 0.9)
-train_data = tokenized_text[:train_size]
-vali_Data = tokenized_text[train_size:]
+train_data = torch.tensor(tokenized_text[:train_size], dtype=torch.long, device=device)
+vali_data = torch.tensor(tokenized_text[train_size:], dtype=torch.long, device=device)
 
 def get_batch(split):
-    data = train_data if split == "train" else vali_Data
+    data = train_data if split == "train" else vali_data
     idxs = torch.randint(low=0,high=len(data)-context_length,size=(batch_size,))
     x = torch.stack([data[idx:idx+context_length] for idx in idxs])
     y = torch.stack([data[idx + 1 : idx + 1 + context_length] for idx in idxs])
@@ -61,6 +62,8 @@ def estimate_loss(model):
 
 
 model = Model().to(device)
+ckpt_path = os.path.join(os.path.dirname(__file__), 'model.ckpt')
+model.load_state_dict(torch.load(ckpt_path, map_location=device))
 optimizer = torch.optim.Adamax(model.parameters(),lr=learning_rate)
 for step in range(max_steps):
     
@@ -77,4 +80,4 @@ for step in range(max_steps):
     
     
     
-torch.save(model.state_dict(),'model.ckpt')
+torch.save(model.state_dict(),'model_v2.ckpt')
